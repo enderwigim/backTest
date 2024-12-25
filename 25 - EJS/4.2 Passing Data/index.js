720ccb4fafd8ce627bc86afc306238028f6b2480
Santiago Lorenzano <enderwigim@gmail.com>
2024-12-25 16:04:07 +0100

    4.2 and 4.3 finished

--- 25 - EJS/4.2 Passing Data/index.js
+++ 25 - EJS/4.2 Passing Data/index.js
@@ -6,10 +6,20 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    countName: null
+  })
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  let fullName = req.body["fName"] + req.body["lName"];
+  res.render("index.ejs", {
+    countName : fullName.length
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
